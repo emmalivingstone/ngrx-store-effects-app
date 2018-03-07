aa62711ec77a5fc04e342c40e2d1f8f5aa9d3452
Emma Livingstone <EmLiv@isherwoods.com>
2018-03-07 12:16:14 +0000

    Linting

--- src/products/store/actions/toppings.action.ts
+++ src/products/store/actions/toppings.action.ts
@@ -1,11 +1,11 @@
-import { Action } from '@ngrx/store';
+import { Action } from "@ngrx/store";
 
-import { Topping } from '../../models/topping.model';
+import { Topping } from "../../models/topping.model";
 
-export const LOAD_TOPPINGS = '[Products] Load Toppings';
-export const LOAD_TOPPINGS_FAIL = '[Products] Load Toppings Fail';
-export const LOAD_TOPPINGS_SUCCESS = '[Products] Load Toppings Success';
-export const VISUALISE_TOPPINGS = '[Products] Visualise Toppings';
+export const LOAD_TOPPINGS = "[Products] Load Toppings";
+export const LOAD_TOPPINGS_FAIL = "[Products] Load Toppings Fail";
+export const LOAD_TOPPINGS_SUCCESS = "[Products] Load Toppings Success";
+export const VISUALISE_TOPPINGS = "[Products] Visualise Toppings";
 
 export class LoadToppings implements Action {
   readonly type = LOAD_TOPPINGS;
